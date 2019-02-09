c08591ef970ab697474907df26380f68baf2601a
Einradritter <hans-konrad.weber@stud.uni-due.de>
2019-02-09 21:42:48 +0100

    i started adding some documentation via roxygen2.

---
+++ R/ggpolymer.R
@@ -0,0 +1,9 @@
+#' ggpolymer: A package containing a collection of useful functions for everyday work in the Gröschel polymer lab.
+#'
+#' Until now the package helps with:
+#' planing ATRP reactions and processing gpc data
+#'
+#' In future i plan on adding: DLS and NMR data processing, and at some point a ATRP database, as well as a shiny app for easy use.
+#' @docType package
+#' @name ggpolymer
+NULL
--- R/gpc_traces.R
+++ R/gpc_traces.R
@@ -1,10 +1,20 @@
-traces_in_dir <- function(dir) {
-  file_list <- list.files("../../data/GPC/hw31-PDMAEMA-kinetik/ascii/", full.names = T)
+#' Read all gpc ascii files in a directory
+#'
+#' @param dir A string.
+#' @return A list of gpc data tibbles as elements.
+traces_in_dir <- function(dir) { # an example should be added to documentation
+  file_list <- list.files(dir, full.names = T)
   traces <- purrr::map(file_list, read_gpc_elu)
   return(traces)
 }
 
-plot_traces <- function(traces, durations, xlim = c(10,45)) {
+#' Plot refractive index vs elution volume from a list of gpc data tibbles
+#'
+#' @param traces A list of gpc data tibbles (best used with traces_in_dir(),
+#' @seealso \code{\link{traces_in_dir}})
+#' @param durations a vector (numeric or string) of reaction durations at sample
+#'   time.
+plot_traces <- function(traces, durations, xlim = c(10,45)) { # example should be added to documentation
   trace_cols <- rainbow(length(traces))
   plot(traces[[1]]$Volume, traces[[1]]$RI, xlim = xlim, type = "l", col = trace_cols[1], lwd = 1)
   for (trace_nr in 2:length(traces)) {
@@ -15,4 +25,6 @@ plot_traces <- function(traces, durations, xlim = c(10,45)) {
   legend("topleft", legend = durations,
     col = trace_cols, lty = 1)
 }
+
+
 
